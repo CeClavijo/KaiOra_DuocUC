c56e2bd17936445e1b0cc86d6966db9565f3f39d
CeClavijo <137459072+CeClavijo@users.noreply.github.com>
2026-04-28 19:42:24 +0000

    login

--- Producto/KaiOra/src/app/shared/components/custom-input/custom-input.component.ts
+++ Producto/KaiOra/src/app/shared/components/custom-input/custom-input.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-input',
   templateUrl: './custom-input.component.html',
   styleUrls: ['./custom-input.component.scss'],
+  standalone: false
 })
 export class CustomInputComponent  implements OnInit {
+
+  @Input() control!: FormControl;
+  @Input() type!: string;
+  @Input() label!: string; 
+  @Input() autocomplete!: string;
+  @Input() icon!: string;
 
   constructor() { }
 
