e2ff09774cdd62c766eb70ddb882a22e3f1e0c8c
MartinMonje00 <165113501+MartinMonje00@users.noreply.github.com>
2026-05-17 19:55:41 +0000

    aplicado mejoras visuales para la pagina principal del profesor

--- Producto/KaiOra/src/app/pages/main/home/home.page.ts
+++ Producto/KaiOra/src/app/pages/main/home/home.page.ts
@@ -67,4 +67,16 @@ export class HomePage implements OnInit {
 
     if (success) this.getRecipes();
   }
+
+  formatViews(views: number) {
+    if (!views) return '0';
+
+    if (views < 1000) {
+      return views.toString()
+    } else if (views >= 1000 && views < 1000000) {
+      return (views / 1000).toFixed(1).replace(/\.0$/, '') + 'k';
+    } else {
+      return (views / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+    }
+  }
 }
